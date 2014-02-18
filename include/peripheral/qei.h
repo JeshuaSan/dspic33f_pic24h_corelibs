a0d92df798d494446af68be99c0523a1f22817dd
Jesus Sanchez <jesus.san.sev@gmail.com>
2014-02-18 08:06:14 -0600

    added jump tables for pin assignments

--- include/peripheral/qei.h
+++ include/peripheral/qei.h
@@ -1,12 +1,24 @@
 #ifndef QEI_H
 #define	QEI_H
 
+#include <stdint.h>
+
+
 enum QEI
 {
     QEI1,
     QEI2
 };
 
-void qei_init(enum QEI qei);
+enum QEI_MODE
+{
+    QEI_2X_INDEX = 4,   /* QEIM<10:8> = 4   QEI 2x mode, reset by index pulse */
+    QEI_2X_MATCH,       /* QEIM<10:8> = 5   QEI 2x mode, reset by match (MAXxCNT) */
+    QEI_4X_INDEX,       /* QEIM<10:8> = 6   QEI 4x mode, reset by index pulse */
+    QEI_4X_MATCH        /* QEIM<10:8> = 7   QEI 4x mode, reset by match (MAXxCNT) */
+};
 
+void qei_init(enum QEI qei, uint8_t a, uint8_t b, enum QEI_MODE md);
+void qei_setMode(enum QEI qei, enum QEI_MODE md);
+uint16_t qei_getCNT(enum QEI qei);
 #endif	/* QEI_H */
--- src/peripheral/qei.c
+++ src/peripheral/qei.c
@@ -1,16 +1,64 @@
 #include "peripheral/qei.h"
-#include "device/pinconfig.h"
 #include "common/globals.h"
 #include "common/types.h"
 #include <pps.h>
 
+/* Private protorypes *********************************************************/
+static inline void qei_pins1(uint8_t a, uint8_t b);
+static inline void qei_pins2(uint8_t a, uint8_t b);
+static vuint16_t *qeiCON[] = {&QEI1CON, &QEI2CON};
+static vuint16_t *qeiCNT[] = {&POS1CNT, &POS2CNT};
 
-void qei_init(enum QEI qei)
+/* Main functions *************************************************************/
+void qei_init(enum QEI qei, uint8_t a, uint8_t b, enum QEI_MODE md)
+{
+    static void(* qei_pins[])(uint8_t a, uint8_t b) = {qei_pins1, qei_pins2};
+
+    
+    qei_pins[qei](a, b);
+
+    *qeiCON[qei] =
+            (0U << 15)  |   /* CNTERR = 0   Position count error has not ocurred
+                                            only applies when QEIM = 110 or 100 */
+                            //              bit 14 unimplemented
+            (0U << 13)  |   /* QEISDL = 0   Continue in idle */
+                            /* INDEX        bit 12 read-only */
+                            /* UPDN         bit 11 read-only when QEIM = 1xx */
+            (md << 8)   |   /* QEIM<10:8> = mode */
+            (0U << 7)   |   /* SWAPAB = 0   Phase A and phase B inputs are not swapped */
+            (1U << 6)   |   /* PCDOUT = 1   QEI logic constrols state of I/O pins */
+                            /* TQGATE       ignored on QEI mode */
+                            /* TQCKPS<4:3>  ignored on QEI mode */
+            (1U << 2);      /* POSRES = 1   Index pulse resets position counter
+                                            applies only when QEIM = 100 or 110 */
+}
+
+void qei_setMode(enum QEI qei, enum QEI_MODE mode)
 {
 
+}
+
+uint16_t qei_getCNT(enum QEI qei)
+{
+    return *qeiCNT[qei];
+}
+
+/* Private functions **********************************************************/
+
+static inline void qei_pins1(uint8_t a, uint8_t b)
+{
     // Map PPS pins to periphereal functions
     PPSUnLock;
-    PPSInput(IN_FN_PPS_QEA1, QE1_A);
-    PPSInput(IN_FN_PPS_QEB1, QE1_B);
+    PPSInput(IN_FN_PPS_QEA1, a);
+    PPSInput(IN_FN_PPS_QEB1, b);
+    PPSLock;
+}
+
+static inline void qei_pins2(uint8_t a, uint8_t b)
+{
+    // Map PPS pins to periphereal functions
+    PPSUnLock;
+    PPSInput(IN_FN_PPS_QEA2, a);
+    PPSInput(IN_FN_PPS_QEB2, b);
     PPSLock;
 }
