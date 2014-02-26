ccd99859d9d16ec6e4231d0088d07165f1146255
Jesus Sanchez <jesus.san.sev@gmail.com>
2014-02-25 19:37:59 -0600

    cleanup

--- include/peripheral/mcpwm.h
+++ include/peripheral/mcpwm.h
@@ -1,6 +1,7 @@
 #ifndef MCPWM_H
 #define	MCPWM_H
 
+#include <xc.h>
 #include <stdint.h>
 
 void mcpwm_init(uint16_t freq);
--- src/peripheral/mcpwm.c
+++ src/peripheral/mcpwm.c
@@ -1,8 +1,9 @@
 #include "peripheral/mcpwm.h"
+#include "common/globals.h"
 
 /* Private prototypes *********************************************************/
 static void mcpwm_configReg();
-static inline void mcpwm_setFreq(uint16_t freq);
+static void mcpwm_setFreq(uint16_t freq);
 
 /* Public functions ***********************************************************/
 void mcpwm_init(uint16_t freq)
@@ -16,11 +17,19 @@ void mcpwm_init(uint16_t freq)
             (0U << 2)   |   /* PTCKPS<3:2> = 0  1:1 prescale */
             (0U << 0);      /* PTMOD<1:0> = 0   PWM time base operates in Free-running mode */
 
-
+    mcpwm_setFreq(freq);
 }
 
 /* Private functions **********************************************************/
-static inline void mcpwm_setFreq(uint16_t freq)
+static void mcpwm_setFreq(uint16_t freq)
 {
+    // PxTPER = (FCY/(Fpwm * PxTMRPS)) - 1
+    uint8_t mcpwmPTCKPS[] = {1,4,16,64};              // Posible prescaler values
+    uint16_t ptckps = P1TCON;          // Actual prescaler value (P1TCON:PTCKPS<3:2>)
+    uint8_t mcpwmTMRPS = mcpwmPTCKPS[ptckps];
 
+    uint32_t pwmFreq = ((uint32_t)freq) * mcpwmTMRPS; // Freq * PxTMRPS
+    uint16_t ptper = (FCY/pwmFreq) - 1;              // (FCY/Fpwm)-1
+
+    P1TPER = ptper;
 }
